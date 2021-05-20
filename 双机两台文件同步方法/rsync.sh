866b8afbd2e263738fd4514f341dd479a3d42ed2
wxy0715 <1743657883@qq.com>
2021-05-20 13:34:39 +0800

    主从复制

--- 双机两台文件同步方法/rsync.sh
+++ 双机两台文件同步方法/rsync.sh
@@ -1,1 +1,1 @@
-rsync -avz --password-file=/etc/rsyncdpwd  lsbljsync@192.168.1.81::common /opt/lsblj/records  >/dev/null 2>&1
+rsync -avz --password-file=/etc/rsyncdpwd --delete /opt/lsblj/records lsbljsync@192.168.1.81::common  >/dev/null 2>&1
